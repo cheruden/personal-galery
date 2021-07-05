dc5d06ae88c1a7d5e2211d287c8dbf5ca22399e8
cheruden <cheruden25@gmail.com>
2021-07-05 08:27:33 +0300

    remove error on my tests

--- personal/tests.py
+++ personal/tests.py
@@ -7,7 +7,7 @@ class GalleryTestClass(TestCase):
         self.new_category.save_category()
         self.new_location = Location(location_name = 'Mombasa')
         self.new_location.save_location()
-        self.new_image = Image(id=1,image_name='learn', image_description='Learn 2017',image_path='media/gallery/dance-3134828_1920.jpg',image_category=self.new_category,image_location=self.new_location)
+        self.new_image = Image(id=1,image_name='learn', image_description='Learn 2021',image_path='media/gallery/dance-3134828_1920.jpg',image_category=self.new_category,image_location=self.new_location)
 
     def tearDown(self):
         Category.objects.all().delete()
